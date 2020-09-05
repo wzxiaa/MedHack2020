ba805556f34ad442c27752370c62724cabb91ee6
Wenzong Xia <wenzong.xia@mail.mcgill.ca>
2020-09-05 11:43:36 -0400

    added calendar UI

--- App.js
+++ App.js
@@ -4,79 +4,63 @@ import { StyleSheet, Text, View, Button } from "react-native";
 
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import {StackNavigator} from "@react-navigation/native"
+import { StackNavigator } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import Icon from "react-native-vector-icons/Ionicons";
-import MainTabScreen from "./screens/MainTabScreen";
 
 import HomeScreen from "./screens/HomeScreen";
-import DetailsScreen from "./screens/DetailsScreen";
-import ProfileScreen from "./screens/ProfileScreen";
 import CalenderScreen from "./screens/CalenderScreen";
 import PatientProfileScreen from "./screens/PatientProfileScreen";
 
 const HomeStack = createStackNavigator();
-const DetailsStack = createStackNavigator();
-// const Drawer = createDrawerNavigator();
-// const Stack = createStackNavigator();
+const CalendarStack = createStackNavigator();
 
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-  <Tab.Navigator initialRouteName="Home" activeColor="#fff" backgroundColor="#009387">
-    <Tab.Screen
-      name="Home"
-      component={HomeStackScreen}
-      options={{
-        tabBarLabel: "All Patients",
-        tabBarColor: "#009387",
-        
-        tabBarIcon: ({ color }) => (
-          <MaterialCommunityIcons name="account-group" color={color} size={26} />
-        ),
-        
-      }}
-    />
-    {/* <Tab.Screen
-      name="Details"
-      component={DetailsStackScreen}
-      options={{
-        tabBarLabel: "Details",
-        tabBarColor: "#009387",
-        tabBarIcon: ({ color }) => (
-          <MaterialCommunityIcons name="bell" color={color} size={26} />
-        ),
-      }}
-    /> */}
-    <Tab.Screen
-      name="Calender"
-      component={CalenderScreen}
-      options={{
-        tabBarLabel: "Calender",
-        tabBarColor: "#009387",
-        tabBarIcon: ({ color }) => (
-          <MaterialCommunityIcons name="calendar-clock" color={color} size={26} />
-        ),
-      }}
-    />
-    {/* <Tab.Screen
-      name="Profile"
-      component={ProfileScreen}
-      options={{
-        tabBarLabel: "Profile",
-        tabBarColor: "#009387",
-        tabBarIcon: ({ color }) => (
-          <MaterialCommunityIcons name="account" color={color} size={26} />
-        ),
-      }}
-    /> */}
-  </Tab.Navigator>
-    </NavigationContainer> 
+      <Tab.Navigator
+        initialRouteName="Home"
+        activeColor="#fff"
+        backgroundColor="#009387"
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeStackScreen}
+          options={{
+            tabBarLabel: "All Patients",
+            tabBarColor: "#009387",
+
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="account-group"
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Calender"
+          component={CalendarStackScreen}
+          options={{
+            tabBarLabel: "Calender",
+            tabBarColor: "#009387",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="calendar-clock"
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -97,48 +81,44 @@ const HomeStackScreen = ({ navigation }) => (
       component={HomeScreen}
       options={{
         title: "All Patients",
-        
-        headerRight: () =>(
-          <Icon.Button 
-          name="ios-add-circle-outline" 
-          backgroundColor="#009387"
-          size={30}
-          onPress={() => navigation.navigate('PatientProfile')}
+
+        headerRight: () => (
+          <Icon.Button
+            name="ios-add-circle-outline"
+            backgroundColor="#009387"
+            size={30}
+            onPress={() => navigation.navigate("PatientProfile")}
           ></Icon.Button>
-        )
+        ),
       }}
     />
     <HomeStack.Screen
       name="PatientProfile"
       component={PatientProfileScreen}
-      options={{title:"Patient Profile"}}
+      options={{ title: "Patient Profile" }}
     />
-    
   </HomeStack.Navigator>
 );
 
-// const DetailsStackScreen = ({ navigation }) => (
-//   <DetailsStack.Navigator
-//     screenOptions={{
-//       headerStyle: {
-//         backgroundColor: "#009387",
-//       },
-//       headerTintColor: "#fff",
-//       headerTitleStyle: {
-//         fontWeight: "bold",
-//       },
-//     }}
-//   >
-//     <DetailsStack.Screen
-//       name="Details"
-//       component={DetailsScreen}
-//       options={{
-        
-//       }}
-//     />
-//   </DetailsStack.Navigator>
-// );
-
+const CalendarStackScreen = ({ navigation }) => (
+  <CalendarStack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: "#009387",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "bold",
+      },
+    }}
+  >
+    <CalendarStack.Screen
+      name="Calendar"
+      component={CalenderScreen}
+      options={{}}
+    />
+  </CalendarStack.Navigator>
+);
 
 const styles = StyleSheet.create({
   container: {
--- screens/CalenderScreen.js
+++ screens/CalenderScreen.js
@@ -1,11 +1,80 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  Alert,
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import {
+  CalendarList,
+  WeekCalendar,
+  Agenda,
+  changeSelectedDateFromCalendar,
+  selectedDateOfTheWeek,
+} from "react-native-calendars";
+
+const timeToString = (time) => {
+  const date = new Date(time);
+  return date.toISOString().split("T")[0];
+};
 
 const CalenderScreen = () => {
+  const [items, setItems] = useState({});
+
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: "Item for " + strTime + " #" + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };
+
+  const renderItem = (item) => {
+    return (
+      <TouchableOpacity
+        style={[styles.item, { height: item.height }]}
+        onPress={() => Alert.alert(item.name)}
+      >
+        <Text>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const renderEmptyDate = () => {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>CalenderScreen</Text>
-      <Button title="Click Here" onPress={() => alert("Button Clicked!")} />
+    <View style={{ flex: 1 }}>
+      <Agenda
+        items={items}
+        loadItemsForMonth={loadItems.bind(this)}
+        renderItem={renderItem.bind(this)}
+        renderEmptyDate={renderEmptyDate.bind(this)}
+        selected={"2017-05-16"}
+      />
     </View>
   );
 };
@@ -13,9 +82,17 @@ const CalenderScreen = () => {
 export default CalenderScreen;
 
 const styles = StyleSheet.create({
-  container: {
+  item: {
+    backgroundColor: "white",
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17,
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30,
   },
 });
